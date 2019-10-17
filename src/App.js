e8494c63901bc7e99f6e747874b156ed572e6b97
Pier Philippon <pier.philippon@gmail.com>
2019-10-17 13:36:30 +0200

    modification class et deplacement style homepage dans component homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./homepage";
-import './homepage.scss'
 
 function App() {
   return (
--- src/homepage.jsx
+++ src/homepage.jsx
@@ -1,35 +1,36 @@
 import React from 'react';
+import './homepage.scss'
 
 const HomePage = () => (
     <div className="homepage">
         <div className="directory-menu">
             <div className="menu-item">
                 <div className="content">
-                    <h1 className="titles">HATS</h1>
+                    <h1 className="title">HATS</h1>
                     <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
             <div className="menu-item">
                 <div className="content">
-                    <h1 className="titles">JACKETS</h1>
+                    <h1 className="title">JACKETS</h1>
                     <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
             <div className="menu-item">
                 <div className="content">
-                    <h1 className="titles">SNEAKERS</h1>
+                    <h1 className="title">SNEAKERS</h1>
                     <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
             <div className="menu-item">
                 <div className="content">
-                    <h1 className="titles">WOMENS</h1>
+                    <h1 className="title">WOMENS</h1>
                     <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
             <div className="menu-item">
                 <div className="content">
-                    <h1 className="titles">MENS</h1>
+                    <h1 className="title">MENS</h1>
                     <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
