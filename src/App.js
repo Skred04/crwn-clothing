151f2ce193fe5b288b53ad1f1b84b1149bcf7c0e
Pier Philippon <pier.philippon@gmail.com>
2019-10-28 23:52:38 +0100

    Ajout page de formulaire + component FormInput + component CustomButton

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
 import Header from "./components/header/header";
+import SignInAndUp from "./pages/sign-in-up/sign-in-up";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Switch>
             <Route exact path='/' component={HomePage}/>
             <Route exact path='/shop' component={ShopPage}/>
+            <Route exact path='/signin' component={SignInAndUp}/>
         </Switch>
     </div>
   );
